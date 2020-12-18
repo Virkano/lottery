d2f2d502e8626a64bc33bb24e00deff4244625f5
Virkano <15737228935@163.com>
2020-12-18 14:29:53 +0800

    test

---
+++ js/member.js
@@ -0,0 +1,106 @@
+var member = [
+  {
+    "phone": "No.2038",
+    "name": "Summer Gong"
+  },
+  {
+    "phone": "No.0282",
+    "name": "Amy Lee"
+  },
+  {
+    "phone": "No.3392",
+    "name": "Brance Ni"
+  },
+  {
+    "phone": "No.8080",
+    "name": "Cami Wang"
+  },
+  {
+    "phone": "No.3855",
+    "name": "Daisy Dai"
+  },
+  {
+    "phone": "No.6389",
+    "name": "Eddie Lee"
+  },
+  {
+    "phone": "No.4440",
+    "name": "Hayley (越南)"
+  },
+  {
+    "phone": "No.3170",
+    "name": "Holnam Sha"
+  },
+  {
+    "phone": "No.1688",
+    "name": "Jackie Hsu"
+  },
+  {
+    "phone": "No.0018",
+    "name": "Jay Hsu"
+  },
+  {
+    "phone": "No.0318",
+    "name": "Joann Cheung"
+  },
+  {
+    "phone": "No.7712",
+    "name": "Kano Zhao"
+  },
+  {
+    "phone": "No.6561",
+    "name": "Mabel Gu"
+  },
+  {
+    "phone": "No.1260",
+    "name": "Mark wang"
+  },
+  {
+    "phone": "No.6280",
+    "name": "Oscar Yeung"
+  },
+  {
+    "phone": "No.8517",
+    "name": "Robin Jiang"
+  },
+  {
+    "phone": "No.2335",
+    "name": "Royde Guan"
+  },
+  {
+    "phone": "No.2681",
+    "name": "Roye Shi"
+  },
+  {
+    "phone": "No.4024",
+    "name": "Ruby Liu"
+  },
+  {
+    "phone": "No.0463",
+    "name": "Sophie Xiao"
+  },
+  {
+    "phone": "No.2423",
+    "name": "Starry Xiao"
+  },
+  {
+    "phone": "No.5357",
+    "name": "Terry Xu"
+  },
+  {
+    "phone": "No.8751",
+    "name": "Vince Chen"
+  },
+  {
+    "phone": "No.0737",
+    "name": "Wendy Huang"
+  },
+  {
+    "phone": "No.5959",
+    "name": "William Zhang"
+  },
+  {
+    "phone": "No.8909",
+    "name": "Zoe Zou"
+  }
+]
